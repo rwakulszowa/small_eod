e00afe36f03702cc85661a213a39304ba3a735f0
rwakulszowa <rwakulszowa1@gmail.com>
2021-07-14 16:24:09 +0200

    Extract Letter.reference_number to a separate model
    Autocompletion logic is designed to work on models. Making
    reference_number a model will let us avoid creating a custom
    autocompletion solution.

--- backend-project/small_eod/letters/models.py
+++ backend-project/small_eod/letters/models.py
@@ -17,6 +17,18 @@ class DocumentType(models.Model):
     )
 
 
+class ReferenceNumber(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name=_("Reference number"),
+        help_text=_("Reference number of letter."),
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
 class Letter(TimestampUserLogModel):
     class Direction(models.TextChoices):
         IN = "IN", "Received"
@@ -53,10 +65,11 @@ class Letter(TimestampUserLogModel):
         help_text=_("Excerpt of letter."),
         blank=True,
     )
-    reference_number = models.CharField(
-        max_length=256,
+    reference_number = models.ForeignKey(
+        to=ReferenceNumber,
+        on_delete=models.DO_NOTHING,
         verbose_name=_("Reference number"),
-        help_text=_("Reference number of letter."),
+        null=True,
         blank=True,
     )
     case = models.ForeignKey(
